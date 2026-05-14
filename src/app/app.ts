d81189e08b5a3f2d89466490e76bdc1c2806dfef
Doug Broad <dcbroad3@gmail.com>
2026-05-14 00:06:01 -0400

    Style tweaks

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.css',
   host: {
-    class: 'w-dvw h-dvh flex flex-col items-center justify-center bg-orange-400 gap-4',
+    class: 'w-dvw h-dvh flex flex-col items-center justify-center bg-orange-400 gap-4 text-center',
   },
 })
 export class App {}
