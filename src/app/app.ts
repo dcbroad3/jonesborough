bc8ba7c9ea466bce24866bfc8c549b68709355b1
Doug Broad <dcbroad3@gmail.com>
2026-05-14 00:01:24 -0400

    Updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +6,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
   host: {
-    class: 'w-dvw h-dvh flex items-center justify-center bg-orange-400'
-  }
+    class: 'w-dvw h-dvh flex flex-col items-center justify-center bg-orange-400 gap-4',
+  },
 })
-export class App {
-}
+export class App {}
